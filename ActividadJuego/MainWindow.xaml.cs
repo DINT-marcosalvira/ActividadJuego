e7e1a34052fffc6c21c3105ded4e915e94ef288c
Marcos Alvira Romero <marcos.alvira@iesdoctorbalmis.com>
2019-10-15 12:49:15 +0200

    Cambiar multiplica tablas

--- ActividadJuego/MainWindow.xaml.cs
+++ ActividadJuego/MainWindow.xaml.cs
@@ -23,6 +23,64 @@ namespace ActividadJuego
         public MainWindow()
         {
             InitializeComponent();
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (RadioButtonBaja.IsChecked == true)
+            {
+                InicioNivel(3);
+            }
+            else if(RadioButtonMedia.IsChecked == true)
+            {
+                InicioNivel(4);
+            }
+            else if(RadioButtonAlta.IsChecked == true)
+            {
+                InicioNivel(5);
+            }
+        }
+
+        public void InicioNivel(int numeroFilas)
+        {
+            for (int i = 0; i < numeroFilas; i++)
+            {
+                GridTabla.RowDefinitions.RemoveAt(i);
+            }
+            for (int y = 0; y < 4; y++)
+            {
+                GridTabla.ColumnDefinitions.RemoveAt(y);
+            }
+
+            for (int i = 0; i < numeroFilas; i++)
+            {
+                GridTabla.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int fila = 0; fila < numeroFilas; fila++)
+            {
+                for (int z = 0; z < 4; z++)
+                {
+                    Border borde = new Border();
+                    Viewbox vb = new Viewbox();
+                    TextBlock tb = new TextBlock();
+                    vb.Child = tb;
+                    borde.Child = vb;
+                    borde.BorderBrush = Brushes.Black;
+                    borde.BorderThickness = new Thickness(3);
+                    borde.Margin = new Thickness(3);
+                    borde.CornerRadius = new CornerRadius(10);
+                    tb.FontFamily = new FontFamily("Webdings");
+                    tb.Text = "s";
+                    borde.Background = Brushes.Beige;
+                    GridTabla.Children.Add(borde);
+                    Grid.SetRow(borde, fila);
+                    Grid.SetColumn(borde, z);
+                }
+                
+            }
         }
     }
 }
