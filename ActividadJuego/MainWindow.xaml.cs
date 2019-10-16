98e9704dc5ec5b1e1e03368eb21967f0506cb41c
Marcos Alvira Romero <marcos.alvira@iesdoctorbalmis.com>
2019-10-16 13:29:56 +0200

    Arreglado TAGS

--- ActividadJuego/MainWindow.xaml.cs
+++ ActividadJuego/MainWindow.xaml.cs
@@ -43,6 +43,76 @@ namespace ActividadJuego
             }
         }
 
+        public List<char> GeneraAleatorio(int numeroFilas)
+        {
+            List<char> listaLetras = new List<char>();
+
+            if (numeroFilas == 3)
+            {
+                Random r = new Random();
+                for (int i = 0; i < 6; i++)
+                {
+                    int letra = r.Next(65,91);
+                    char l = (char)letra;
+                    listaLetras.Add(l);
+                }
+                //Añade la pareja de cada letra
+                for (int i = 0; i < 6; i++)
+                {
+                    listaLetras.Add(listaLetras[i]);
+                }
+            }
+            else if(numeroFilas == 4)
+            {
+                Random r = new Random();
+                for (int i = 0; i < 8; i++)
+                {
+                    int letra = r.Next(65, 91);
+                    char l = (char)letra;
+                    listaLetras.Add(l);
+                }
+
+                for (int i = 0; i < 6; i++)
+                {
+                    listaLetras.Add(listaLetras[i]);
+                }
+            }
+            else if(numeroFilas == 5)
+            {
+                Random r = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    int letra = r.Next(65, 91);
+                    char l = (char)letra;
+                    listaLetras.Add(l);
+                }
+
+                for (int i = 0; i < 6; i++)
+                {
+                    listaLetras.Add(listaLetras[i]);
+                }
+            }
+            listaLetras = DesordenarList(listaLetras);
+            return listaLetras;
+        }
+
+        public List<char> DesordenarList(List<char> lista)
+        {
+            //Desordenar la lista y posiciones
+            List<char> arr = lista;
+            List<char> arrDes = new List<char>();
+
+            Random randNum = new Random();
+            while (arr.Count > 0)
+            {
+                int val = randNum.Next(0, arr.Count - 1);
+                arrDes.Add(arr[val]);
+                arr.RemoveAt(val);
+            }
+
+            return arrDes;
+        }
+
         public void InicioNivel(int numeroFilas)
         {
             GridTabla.RowDefinitions.Clear();
@@ -51,10 +121,12 @@ namespace ActividadJuego
             {
                 GridTabla.RowDefinitions.Add(new RowDefinition());
             }
+            //Con la lista desordenada y un contador, añadimos al tag toda la lista.
+            List<char> lista = GeneraAleatorio(numeroFilas);
 
-            for (int fila = 0; fila < numeroFilas; fila++)
+            for (int fila = 0, contadorLista = 0; fila < numeroFilas; fila++)
             {
-                for (int z = 0; z < 4; z++)
+                for (int z = 0; z < 4; contadorLista++ ,z++)
                 {
                     Border borde = new Border();
                     Viewbox vb = new Viewbox();
@@ -67,12 +139,12 @@ namespace ActividadJuego
                     borde.CornerRadius = new CornerRadius(10);
                     tb.FontFamily = new FontFamily("Webdings");
                     tb.Text = "s";
-                    borde.Background = LinearGradientBrush(); Brushes.Beige;
+                    tb.Tag = lista[contadorLista];
+                    borde.Background = Brushes.Beige;
                     GridTabla.Children.Add(borde);
                     Grid.SetRow(borde, fila);
                     Grid.SetColumn(borde, z);
                 }
-                
             }
         }
     }
